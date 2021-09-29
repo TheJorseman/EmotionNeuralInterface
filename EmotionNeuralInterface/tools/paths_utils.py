227275e6fbd0f0553d45f02083126ead0828cc92
miguel17546mals <miguel17546mals@gmail.com>
2021-09-29 16:18:15 -0500

    [MOD] new architectures

--- EmotionNeuralInterface/tools/paths_utils.py
+++ EmotionNeuralInterface/tools/paths_utils.py
@@ -1,4 +1,7 @@
 import os
+import re
+import shutil
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
 def get_paths_experiment(root_path, accepted_files=(".csv")):
   """
@@ -19,4 +22,34 @@ def get_paths_experiment(root_path, accepted_files=(".csv")):
       data[dir] = [os.path.join(data_path,file) for file in os.listdir(data_path) if file.lower().endswith(accepted_files)]
   return data
 
+def get_path(data_str):
+  regex = re.compile(
+      r"(\w+://)?"                # protocol                      (optional)
+      r"(\w+\.)?"                 # host                          (optional)
+      r"((\w+)\.(\w+))"           # domain
+      r"(\.\w+)*"                 # top-level domain              (optional, can have > 1)
+      r"([\w\-\._\~/]*)*(?<!\.)"  # path, params, anchors, etc.   (optional)
+  )
+  if regex.match(data_str):
+    return download_gdrive_data(data_str)
+  return data_str
+
+def download_gdrive_data(url, default_data_folder="./data"):
+  if data_exist(default_data_folder):
+    return default_data_folder
+  os.mkdir(default_data_folder)
+  filename = "experimento.zip"
+  gdd.download_file_from_google_drive(file_id=url.split("/")[5],
+                                      dest_path=os.path.join(default_data_folder, filename),
+                                      unzip=True)
+  return default_data_folder
+
+def data_exist(path):
+  if not os.path.exists(path):
+    return False
+  file_list = get_paths_experiment(path)
+  if len(file_list) > 0:
+    return True
+  shutil.rmtree(path)
+  return False
   
